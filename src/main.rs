b4aee8ea584792bd285dff6a0c65d2bd1395ebaf
Ali-0211-TR <xgamexarkon@gmail.com>
2024-06-02 22:05:24 +0500

    New Signal service. Started testing period.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,107 @@
-use axum::{routing::post, Json, Router};
-use serde::{Deserialize, Serialize};
-use tokio::{net::TcpListener, sync::broadcast};
-use webrtc::{ice_transport::ice_candidate::RTCIceCandidateInit, peer_connection::sdp::session_description::RTCSessionDescription};
+//! Пишем сигнальный сервер для того чтобы устроить обмен между
+//! двумя устройствами SDP, тем временем настроив их рукапожатие
+//! TODO: пишем вебсокетный сервер
+//! маршруты:
+//! * /answer - сторона которая будет стримить
+//! * /offer - сторона которая будет наблюдать
+//! 
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-struct SignalMessage{
-    sdp: Option<RTCSessionDescription>,
-    candidate: Option<RTCIceCandidateInit>
+
+use std::{collections::HashMap, sync::Arc};
+
+use axum::{extract::{ws::WebSocket, ConnectInfo, Path, WebSocketUpgrade}, http::StatusCode, response::IntoResponse, routing::{get, post}, serve, Json, Router};
+use serde::{Deserialize, Serialize};
+use tokio::{net::{unix::SocketAddr, TcpListener}, sync::Mutex};
+use webrtc::{api::media_engine::MediaEngine, ice_transport::ice_candidate::RTCIceCandidateInit, peer_connection::{configuration::RTCConfiguration, sdp::session_description::RTCSessionDescription}};
+
+#[derive(Clone)]
+struct AppState {
+    cameras: HashMap<String, RTCSessionDescription>,
+    offer: HashMap<String, RTCSessionDescription>
 }
 
+
+
+#[derive(Serialize, Deserialize, Clone)]
+struct SignalMessage {
+
+    sdp: RTCSessionDescription,
+    candidate: RTCIceCandidateInit,
+}
+
+#[derive(Default)]
+struct State {
+    sdp: Option<SignalMessage>,
+    ice_candidates: Vec<RTCIceCandidateInit>,
+}
+type AppState1 = Arc<Mutex<HashMap<String, State>>>;
 #[tokio::main]
 async fn main() {
-    let (tx, _rx) = broadcast::channel::<SignalMessage>(10);
-
-    let app = Router::new()
-        .route("/signal", post(signal_handler))
-        .layer(axum::extract::Extension(tx));
-
-    let addr = TcpListener::bind("0.0.0.0:3030").await.unwrap();//SocketAddr::from(([127, 0, 0, 1], 3030));
-    println!("Signal server running on 0.0.0.0:3030");
-    axum::serve(addr, app)
-        .await
-        .unwrap()
+    let db: HashMap<String, State> = HashMap::new(); // camera_id <-> SDP 
+    let state = Arc::new(Mutex::new(db));
+    let listener = TcpListener::bind("0.0.0.0:3030").await.unwrap();
+    let route = Router::new()
+        .route("/offer/:camera_id", post(offer_handler))
+        .route("/answer/:camera_id", post(answer_handler))
+        .route("/signal/:camera_id", get(get_signal))
+        .layer(axum::extract::Extension(state))
     ;
+
+    serve(listener,route).await.unwrap();
 }
-
-
-async fn signal_handler(
-    axum::extract::Extension(tx): axum::extract::Extension<broadcast::Sender<SignalMessage>>,
-    Json(message): Json<SignalMessage>,
-) -> Json<SignalMessage> {
-    let mut rx = tx.subscribe();
-
-    if message.sdp.is_some() || message.candidate.is_some() {
-        tx.send(message.clone()).unwrap();
+async fn offer_handler(
+    Path(camera_id): Path<String>,
+    axum::extract::Extension(state): axum::extract::Extension<Arc<Mutex<AppState>>>,
+    Json(sdp): Json<RTCSessionDescription>
+)->impl IntoResponse{
+    let mut state = state.lock().await;
+    if let Some(s) = state.cameras.get(&camera_id){
+        let s = s.clone();
+        let _ = state.offer.insert(camera_id, sdp);
+        Json(s).into_response()
+    } else {
+        StatusCode::NOT_FOUND.into_response()
     }
-
-    let response = rx.recv().await.unwrap();
-    Json(response)
 }
+async fn get_signal(
+    Path(camera_id): Path<String>,
+    axum::extract::Extension(state): axum::extract::Extension<Arc<Mutex<AppState>>>,
+) -> impl IntoResponse{
+    let mut state = state.lock().await;
+    if let Some(sdp) = state.offer.get(&camera_id){
+        Json(sdp.clone()).into_response()
+    } else {
+        StatusCode::NOT_FOUND.into_response()
+    }
+}
+async fn answer_handler(
+    Path(camera_id): Path<String>,
+    axum::extract::Extension(state): axum::extract::Extension<Arc<Mutex<AppState>>>,
+    Json(sdp): Json<RTCSessionDescription>
+){
+    let mut state = state.lock().await;
+    state.cameras.insert(camera_id, sdp);
+
+}
+// async fn ws_handler(
+//     ws: WebSocketUpgrade,
+//     user_agent: Option<TypedHeader<headers::UserAgent>>,
+//     ConnectInfo(addr): ConnectInfo<SocketAddr>,
+// ) -> impl IntoResponse {
+//     let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+//         user_agent.to_string()
+//     } else {
+//         String::from("Unknown browser")
+//     };
+//     println!("`{user_agent}` at {addr} connected.");
+//     // finalize the upgrade process by returning upgrade callback.
+//     // we can customize the callback by sending additional info such as address.
+//     ws.on_upgrade(move |socket| handle_socket(socket, addr))
+// }
+
+// async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
+
+// }
+
+
+
